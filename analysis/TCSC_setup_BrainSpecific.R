81c35ee8ceea960459f6e2447bb79fce06e3fc38
Tiffany Amariuta <tiffanyamariuta@g.harvard.edu>
2023-02-06 16:08:02 -0500

    Update TCSC_setup_BrainSpecific.R

--- analysis/TCSC_setup_BrainSpecific.R
+++ analysis/TCSC_setup_BrainSpecific.R
@@ -3,12 +3,12 @@
 library(data.table)
 library(Hmisc)
 
-gtex <- fread("analysis/gene_annotation.txt.gz", header = F, sep = "\t")
+gtex <- fread("TCSC/analysis/gene_annotation.txt.gz", header = F, sep = "\t")
 
-y <- fread("analysis/TissueGroups.txt", header = T)
+y <- fread("TCSC/analysis/TissueGroups.txt", header = T)
 tissues <- y$Tissues
 
-X <- as.matrix(fread("coregulation_scores/CoregulationMatrix_allGTEx_030422.txt.gz", header = F))
+X <- as.matrix(fread("TCSC/coregulation_scores/CoregulationMatrix_allGTEx_030422.txt.gz", header = F))
 
 transcripts <- c()
 count <- 0
@@ -17,14 +17,14 @@ chrs <- c()
 tissueassign <- c()
 for (i in 1:length(tissues)){
 tissue <- tissues[i]
-transcript_key <- fread(paste0("weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
+transcript_key <- fread(paste0("TCSC/weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
 m <- match(transcript_key,gtex$V7)
 genetype <- gtex$V8[m]
 transcript_key <- transcript_key[which(genetype == "protein_coding")]
 transcripts <- c(transcripts,transcript_key)
 tissueassign <- c(tissueassign, rep(i,length(transcript_key)))
 
-gene_annot <- fread(paste0("weights/allEUR_tissues/v8_allEUR_",tissue,"_blup.pos"), header = T)
+gene_annot <- fread(paste0("TCSC/weights/allEUR_tissues/v8_allEUR_",tissue,"_blup.pos"), header = T)
 gene_annot_transcript <- gene_annot$ID
 m <- match(transcript_key,gene_annot_transcript)
 starts <- c(starts,gene_annot$P0[m])
@@ -43,5 +43,5 @@ transcripts <- transcripts[wkeepX]
 chrs <- chrs[wkeepX]
 starts <- starts[wkeepX]
 
-save(list=c("wkeepX","X","transcripts","starts","chrs","tissueassign"), file = "analysis/InputCoreg_BrainSpecific.RData")
+save(list=c("wkeepX","X","transcripts","starts","chrs","tissueassign"), file = "TCSC/analysis/InputCoreg_BrainSpecific.RData")
 
