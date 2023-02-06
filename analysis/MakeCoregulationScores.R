b043b741b5d447af57f773b5df74283d17bdb66b
Tiffany Amariuta <tiffanyamariuta@g.harvard.edu>
2023-02-06 16:01:14 -0500

    Update MakeCoregulationScores.R
    fixed typo in paths

--- analysis/MakeCoregulationScores.R
+++ analysis/MakeCoregulationScores.R
@@ -16,16 +16,16 @@ tissues <- unique(y$MetaTissue)
 small_tissues <- c(3,5,11,12,14,22,24,27:30,33:35,37:38) #add 1 to values 10 and above; update June 21st for artery aorta
 normal_tissues <- c(1:length(tissues))[-small_tissues]
 
-if(i %in% small_tissues){load(paste0("predicted_expression_1KG/allEUR/designmat_",tissues[i],"_v8_allEUR_double.RData"))}else{
-load(paste0("predicted_expression_1KG/320EUR/designmat_",tissues[i],"_v8_320EUR_double.RData"))}
+if(i %in% small_tissues){load(paste0("TCSC/predicted_expression/Nall/designmat_",tissues[i],"_v8_allEUR_double.RData"))}else{
+load(paste0("TCSC/predicted_expression/N320/designmat_",tissues[i],"_v8_320EUR_double.RData"))}
 print(dim(df1))
 df1_focal <- df1
 rm("df1")
-if(i %in% small_tissues){transcript_key <- fread(paste0("weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
-keeptranscripts <- fread(paste0("weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model_keep.txt"), header = F)$V1
+if(i %in% small_tissues){transcript_key <- fread(paste0("TCSC/weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
+keeptranscripts <- fread(paste0("TCSC/weights/heritablegenes/Nall/TranscriptsIn",tissues[i],"Model_keep.txt"), header = F)$V1
 }else{
-transcript_key <- fread(paste0("weights/heritablegenes/N320/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
-keeptranscripts <- fread(paste0("weights/heritablegenes/N320/TranscriptsIn",tissues[i],"Model_keep.txt"), header = F)$V1
+transcript_key <- fread(paste0("TCSC/weights/heritablegenes/N320/TranscriptsIn",tissues[i],"Model.txt"), header = F)$V1
+keeptranscripts <- fread(paste0("TCSC/weights/heritablegenes/N320/TranscriptsIn",tissues[i],"Model_keep.txt"), header = F)$V1
 }
 
 #keep genes with gcta p < 0.01 aaaaand h2 estimate > 0
@@ -44,7 +44,7 @@ focal_tissue_gene_ref <- focal_tissue_gene_ref[keepgenes_focal,]
 
 for (j in 1:length(tissues)){
 if(j == i){ #apply bias correction
-acc <- fread(paste0("weights/accuracies/N320_smalltoo_accuracies_cish2_fromall_",tissues[i],".txt.gz"), header = F)
+acc <- fread(paste0("TCSC/weights/accuracies/N320_smalltoo_accuracies_cish2_fromall_",tissues[i],".txt.gz"), header = F)
 acc <- acc$V2[match(transcript_key,acc$V1)]
 
 genegenecoreg_pc <- c() #vector of length g in focal tissue 
@@ -64,20 +64,20 @@ coreg_before_sum_pc <- cor_est_pc^2
 mysum <- sum(coreg_before_sum_pc) - 1 + acc[k] 
 genegenecoreg_pc <- c(genegenecoreg_pc, mysum)
 }
-file=paste0("coregulation_scores/CoRegScores_v8_320EUR_smalltoo_",tissues[i],"_with_",tissues[j],".txt")
+file=paste0("TCSC/coregulation_scores/CoRegScores_v8_320EUR_smalltoo_",tissues[i],"_with_",tissues[j],".txt")
 write.table(genegenecoreg_pc, file=file, row.names = F, col.names = F, sep = "\t", quote = F)
 system(paste0("gzip ", file,))
 }else{ #target tissue is different from focal; normal co-regulation w/o bias correction
 
 
-if(j %in% small_tissues){load(paste0("predicted_expression_1KG/allEUR/designmat_",tissues[j],"_v8_allEUR_double.RData"))}else{
-load(paste0("predicted_expression_1KG/320EUR/designmat_",tissues[j],"_v8_320EUR_double.RData"))}
+if(j %in% small_tissues){load(paste0("TCSC/predicted_expression/Nall/designmat_",tissues[j],"_v8_allEUR_double.RData"))}else{
+load(paste0("TCSC/predicted_expression/N320/designmat_",tissues[j],"_v8_320EUR_double.RData"))}
 
-if(j %in% small_tissues){transcript_key_other <- fread(paste0("weights/heritablegenes/Nall/TranscriptsIn",tissues[j],"Model.txt"), header = F)$V1
-keeptranscripts <- fread(paste0("weights/heritablegenes/Nall/TranscriptsIn",tissues[j],"Model_keep.txt"), header = F)$V1
+if(j %in% small_tissues){transcript_key_other <- fread(paste0("TCSC/weights/heritablegenes/Nall/TranscriptsIn",tissues[j],"Model.txt"), header = F)$V1
+keeptranscripts <- fread(paste0("TCSC/weights/heritablegenes/Nall/TranscriptsIn",tissues[j],"Model_keep.txt"), header = F)$V1
 }else{
-transcript_key_other <- fread(paste0("weights/heritablegenes/N320/TranscriptsIn",tissues[j],"Model.txt"), header = F)$V1
-keeptranscripts <- fread(paste0("weights/heritablegenes/N320/TranscriptsIn",tissues[j],"Model_keep.txt"), header = F)$V1
+transcript_key_other <- fread(paste0("TCSC/weights/heritablegenes/N320/TranscriptsIn",tissues[j],"Model.txt"), header = F)$V1
+keeptranscripts <- fread(paste0("TCSC/weights/heritablegenes/N320/TranscriptsIn",tissues[j],"Model_keep.txt"), header = F)$V1
 }
 
 w <- which(transcript_key_other %in% keeptranscripts)
@@ -107,7 +107,7 @@ coreg_before_sum_pc <- cor_est_pc^2
 genegenecoreg_pc <- c(genegenecoreg_pc, sum(coreg_before_sum_pc))
 }
 
-file=paste0("coregulation_scores/CoRegScores_v8_320EUR_smalltoo_",tissues[i],"_with_",tissues[j],".txt")
+file=paste0("TCSC/coregulation_scores/CoRegScores_v8_320EUR_smalltoo_",tissues[i],"_with_",tissues[j],".txt")
 write.table(genegenecoreg_pc, file=file, row.names = F, col.names = F, sep = "\t", quote = F)
 system(paste0("gzip ", file,))
 
